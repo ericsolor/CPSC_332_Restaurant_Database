81cea2ba1e341f74a318b8f459500075df0c6db9
Eric Solorzano <ericsolorzano53@gmail.com>
2026-05-04 00:35:39 -0700

    Added the other two migration files required

---
+++ migrations/002_seed_data.sql
@@ -0,0 +1,10 @@
+/*
+    Contains INSERT statements to seed your database.
+
+Minimum requirement:
+
+    At least 5 rows per table (every table)
+    Data needs to make sense
+
+Your sample data should be believable and allow queries to show meaningful results.
+*/
