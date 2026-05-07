bfbe02089eb03666153fa344accdfef3b92768e9
Eric Solorzano <ericsolorzano53@gmail.com>
2026-05-06 22:08:25 -0700

    Removed commented instructions at top of the page

--- migrations/002_seed_data.sql
+++ migrations/002_seed_data.sql
@@ -1,15 +1,3 @@
-/*
-    Contains INSERT statements to seed your database.
-
-    Minimum requirement:
-
-    At least 5 rows per table (every table)
-    Data needs to make sense
-
-    Your sample data should be believable and allow queries to show meaningful results.
-*/
-
-
 -- Insert data into Order_Status table
 INSERT INTO "Order_Status" ("status_name") VALUES
 ('pending'),
--- migrations/003_queries_examples.sql
+++ migrations/003_queries_examples.sql
@@ -1,27 +1,3 @@
-/*
-    This file demonstrates your database operations.
-
-Minimum requirement:
-
-    Include TRIGGER: at least 1 trigger
-    Include VIEW: at least 1 view
-    Include READ (SELECT): at least 6 queries including JOINs
-    Include UPDATE: at least 6 queries
-    Include DELETE: at least 6 queries
-
-Total minimum: 19 queries.
-
-Each query must include a short comment describing what it does, for example:
-
-    -- READ: Show all overdue payments with tenant + unit
-
-    -- UPDATE: Update customer name
-
-    -- DELETE: Remove a canceled reservation
-
-Queries should use multiple tables when appropriate (joins) and reflect realistic business questions.
-*/
-
 -- TRIGGER: Prevent reservations where the party size is larger than the table capacity
 CREATE OR REPLACE FUNCTION check_reservation_capacity()
 RETURNS TRIGGER AS $$
